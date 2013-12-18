893105fec54a813bf01d1c9e6ae0b8fe74cb46ec
seyrenhe <seyrenhe@gmail.com>
2013-12-18 17:05:11 +0800

    add sth

--- src/blog/apps/Blog/views.py
+++ src/blog/apps/Blog/views.py
@@ -5,47 +5,7 @@ from forms import BlogsForm
 def test():
     blogs = functions.get_model('blogs')
     blog = blogs.all().order_by(blogs.c.id.desc())
-    form = BlogsForm()
-    if request.method == 'POST':
-        flag = form.validate(request.params)
-        if flag:
-            n = blogs(**form.data)
-            n.username = request.user.username
-            n.save()
-    return {'blog': blog, 'form': form}
-
-
-
-@expose('/delete/<id>')
-def delete(id):
-        functions.require_login()
-
-        blogs = functions.get_model('blogs')
-        n = blogs.get(blogs.c.id == id)
-        if n:
-                n.delete()
-        return redirect('/')
-
-
-@expose('/edit/<id>')
-def edit(id):
-        blogs = functions.get_model('blogs')
-        if functions.require_login():
-                return redirect(url_for(login))
-        if request.method == 'GET':
-                p = blogs.get(blogs.c.id==id)
-                form = BlogsForm(data={'title':p.title,'content':p.content})
-                return {'form':form}
-        elif request.method == 'POST':
-                form = BlogsForm()
-                flag = form.validate(request.params)
-                n = blogs.get(blogs.c.id == id)
-                if n:
-                        n.username = request.user.username
-                        n.title    = form.data.title
-                        n.content  = form.data.content
-                        n.save()
-                return redirect('/')
+    return {'blog': blog}
 
 
 @expose('/reader/<id>')
@@ -62,15 +22,68 @@ def detail(id):
         else:
             return redirect('/')
 
-
 @expose('/admin')
- class AdminView(object):
-#     def __begin__(self):
-#         functions.require_login()
-#         if not request.user.is_superuser:
-#             error('you have no permisstion to visit the page')
-
+class Admin(object):
 
     @expose('')
     def index(self):
         return {}
+
+    def new_post(self):
+        from forms import BlogsForm
+        import datetime
+
+        Post = functions.get_model('blogs')
+        form = BlogsForm()
+        if request.method == 'GET':
+            return {'form': form}
+        if request.method == 'POST':
+            f
+
+
+# @expose('/delete/<id>')
+# def delete(id):
+#         functions.require_login()
+#
+#         blogs = functions.get_model('blogs')
+#         n = blogs.get(blogs.c.id == id)
+#         if n:
+#                 n.delete()
+#         return redirect('/')
+#
+#
+# @expose('/edit/<id>')
+# def edit(id):
+#         blogs = functions.get_model('blogs')
+#         if functions.require_login():
+#                 return redirect(url_for(login))
+#         if request.method == 'GET':
+#                 p = blogs.get(blogs.c.id==id)
+#                 form = BlogsForm(data={'title':p.title,'content':p.content})
+#                 return {'form':form}
+#         elif request.method == 'POST':
+#                 form = BlogsForm()
+#                 flag = form.validate(request.params)
+#                 n = blogs.get(blogs.c.id == id)
+#                 if n:
+#                         n.username = request.user.username
+#                         n.title    = form.data.title
+#                         n.content  = form.data.content
+#                         n.save()
+#                 return redirect('/')
+
+
+
+
+
+# @expose('/admin')
+#  class Admin(object):
+# #     def __begin__(self):
+# #         functions.require_login()
+# #         if not request.user.is_superuser:
+# #             error('you have no permisstion to visit the page')
+#
+#
+#     @expose('')
+#     def index(self):
+#         return {}
